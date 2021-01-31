2952f5c67e7f1e17de4849b5b0bfd46abee77591
samneter <samneter@gmail.com>
2021-01-31 09:08:46 +0000

    Updating routes.rb file with rules for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-<<<<<<< HEAD
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-=======
->>>>>>> 101810cab448b48235fda6d854aac5476a82426b
+  get  'static_pages/home'
+  get  'static_pages/help'
   root 'application#hello'
-  end
 end
